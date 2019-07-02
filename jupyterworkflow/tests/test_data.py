cc97c3d6166e7d917099fdcd9e227371c11c5521
Chintan Raiyani <chintan.raiyani@sap.com>
2019-07-02 23:17:09 +0530

    BUG: fix bug in date parsing

--- jupyterworkflow/tests/test_data.py
+++ jupyterworkflow/tests/test_data.py
@@ -2,8 +2,10 @@
 
 from jupyterworkflow.data import get_fremont_data
 import pandas as pd
+import numpy as np
 
 def test_fremont_data():
-    data=get_fremont_data()
-    assert all(data.columns==['East', 'West', 'Total'])
-    assert isinstance(data.index,pd.DatetimeIndex)
+    df=get_fremont_data()
+    assert all(df.columns==['East', 'West', 'Total'])
+    assert isinstance(df.index,pd.DatetimeIndex)
+    assert len(np.unique(df.index.time))==24
