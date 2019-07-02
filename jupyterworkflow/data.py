9af847de766feaae60aeec0650f904815cdeea4c
Chintan Raiyani <chintan.raiyani@sap.com>
2019-07-02 21:48:52 +0530

    Refactor data download

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -23,7 +23,13 @@ def get_fremont_data(filename='Fremont.csv',url=FREMONT_URL, force_download=Fals
     """
     if force_download or not os.path.exists(filename):
         urlretrieve(URL,filename)
-    df=pd.read_csv(filename,parse_dates=True,index_col='Date')
+    df=pd.read_csv(filename, index_col='Date')
+    
+    try:
+        df.index = pd.to_datetime(df.index, format='%m/%d/%Y %I:%M:%S %p')
+    except TypeError:
+        df.index = pd.to_datetime(df.index)
+        
     df.columns=['East','West']
     df['Total']=df.East+df.West
     return df
